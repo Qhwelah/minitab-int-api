4a44706583b77d009853a040080e79a72b883dee
qhwelah <jdcooper814@gmail.com>
2026-05-14 18:50:27 -0400

    feat: working startup and shutdown

---
+++ shutdown.sh
@@ -0,0 +1,3 @@
+# Gracefully stop and remove api-app container
+docker stop api-app
+docker rm api-app
--- startup.sh
+++ startup.sh
@@ -11,6 +11,12 @@ fi
 
 echo "Starting api service on port $PORT"
 
-# docker build -t minitab-api .
+# Building with image tag "minitab-api"
+docker build -t minitab-api .
 
-# docker run --name api-app -p 80:80 -d minitab-api:latest
+# Running with container name "api-app" on specified port number
+docker run --name api-app -p $PORT:80 -d minitab-api:latest
+
+echo "Started service name 'api-app' on port number $PORT."
+echo "Go to http://localhost:$PORT/docs for the API swagger page."
+echo "Run 'bash shutdown.sh' when completed to stop and delete container."
