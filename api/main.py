3dfa0e04047bf08861d12b3298cd8bff7f05cbed
qhwelah <jdcooper814@gmail.com>
2026-05-14 18:23:13 -0400

    chore: remove testing get and post endpoints

--- api/main.py
+++ api/main.py
@@ -4,20 +4,14 @@ import numpy as np
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello, world!"}
-
-@app.post("/numbers/")
-def post_numbers(data: List[int]):
-    return {"sum": sum(data)}
-
+# From a list of numbers, calculate and return the mean
 @app.post("/mean")
 def post_mean(list_num: List[float]):
     mean = sum(list_num) / len(list_num)
     mean = round(mean, 3)
     return {"mean": mean}
 
+# From a list of numbers, calculate and return the standard deviation
 @app.post("/stddev")
 def post_stddev(list_num: List[float]):
     stddev = np.std(list_num)
