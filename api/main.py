936454c514a55e14e496d9f6239bdbe2cf506c59
qhwelah <jdcooper814@gmail.com>
2026-05-14 18:05:55 -0400

    feat: working post mean

--- api/main.py
+++ api/main.py
@@ -10,3 +10,9 @@ def home():
 @app.post("/numbers/")
 def post_numbers(data: List[int]):
     return {"sum": sum(data)}
+
+@app.post("/mean/")
+def post_mean(list_num: List[int]):
+    mean = sum(list_num) / len(list_num)
+    mean = round(mean, 3)
+    return {"mean": mean}
