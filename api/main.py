c2b4f1bcbe3234244865b7a445fdfc0d0abdd610
qhwelah <jdcooper814@gmail.com>
2026-05-14 17:33:26 -0400

    feat: accepts numbers as list of ints

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+# from pydantic import BaseModel
+from typing import List
+
+# class NumberList(BaseModel):
+#     numbers: List[int]
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello, world!"}
+
+@app.post("/numbers/")
+def post_numbers(data: List[int]):
+    return {"sum": sum(data)}
