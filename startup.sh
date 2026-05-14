f7981290c7769b9c45d237972bdca3d6997ab709
qhwelah <jdcooper814@gmail.com>
2026-05-14 18:35:38 -0400

    feat: validates port number

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Received port number $1"
+if [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Starting api service on port $1"
+else
+    echo "ERROR: Received argument '$1' which is not a positive integer. Please run script again with a valid integer port number."
+fi
 
 # docker build -t minitab-api .
 
