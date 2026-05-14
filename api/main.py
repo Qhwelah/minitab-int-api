38e2bfc69a7e483952d722335aab5c6c45b9ad60
Qhwelah <120686368+Qhwelah@users.noreply.github.com>
2026-05-14 17:59:28 -0400

    Feature/api accepts lists (#3)
    * feat: accepts numbers as list of ints
    
    * chore: remove non needed pydantic
    
    * docs: usage instructions

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello, world!"}
+
+@app.post("/numbers/")
+def post_numbers(data: List[int]):
+    return {"sum": sum(data)}
