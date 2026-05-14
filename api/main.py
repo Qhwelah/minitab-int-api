ad2c5ad069c14f78b99478372b162b84f5e2e294
Qhwelah <120686368+Qhwelah@users.noreply.github.com>
2026-05-14 18:26:07 -0400

    Feature/mean stddev (#4)
    * feat: working post mean
    
    * feat: added post stddev
    
    * fix: add numpy import
    
    * fix: allow numbers to be floats instead of int only
    
    * fix: remove trailing slash from posts
    
    * chore: remove testing get and post endpoints

--- api/main.py
+++ api/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from typing import List
+import numpy as np
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Hello, world!"}
+# From a list of numbers, calculate and return the mean
+@app.post("/mean")
+def post_mean(list_num: List[float]):
+    mean = sum(list_num) / len(list_num)
+    mean = round(mean, 3)
+    return {"mean": mean}
 
-@app.post("/numbers/")
-def post_numbers(data: List[int]):
-    return {"sum": sum(data)}
+# From a list of numbers, calculate and return the standard deviation
+@app.post("/stddev")
+def post_stddev(list_num: List[float]):
+    stddev = np.std(list_num)
+    stddev = round(stddev, 3)
+    return {"stddev": stddev}
