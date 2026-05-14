4d274452184658de97795940e999c87bb4f247f0
qhwelah <jdcooper814@gmail.com>
2026-05-14 18:42:57 -0400

    feat: requires PORT env var to be set

--- startup.sh
+++ startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [[ $1 =~ ^[0-9]+$ ]]; then
-    echo "Starting api service on port $1"
-else
-    echo "ERROR: Received argument '$1' which is not a positive integer. Please run script again with a valid integer port number."
+# Validate port input number
+if [ -z "$PORT" ]; then
+    echo "ERROR: Environment variable PORT is not set. Please set PORT to some positive integer port number and run the script again."
+    exit 1
+elif ! [[ $PORT =~ ^[0-9]+$ ]]; then
+    echo "ERROR: Received argument '$PORT' which is not a positive integer. Please run script again with a valid integer port number."
+    exit 1
 fi
+
+echo "Starting api service on port $PORT"
 
 # docker build -t minitab-api .
 
